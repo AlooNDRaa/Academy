029ebc6b7254d507e5474837c703064e99abe30e
Alondra <alogadea@emeal.nttdata.com>
2024-11-21 09:43:13 -0300

    Fix errors in style.css

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
 document.getElementById("header").innerHTML = `
-  <header class="head">
-    <h2 class="title">Mystique<span class="span-title">Shop</span></h2>
-    <input type="search" placeholder="Busca productos, marcas y más" id="Buscador" />
-    <nav class="navbar-home">
-        <ul class="ul-nav">
-            <li><a href="./index.html">Inicio</a></li>
-            <li><a href="./html/helpcenter.html">Soporte</a></li>
-            <li class="nav-item dropdown">
-                <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    Artículos
-                </button>
-                <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="./html/Cards.html">Moda</a></li>
-                    <li><a class="dropdown-item" href="#">Joyas</a></li>
-                    <li><a class="dropdown-item" href="#">Accesorios y bolsos</a></li>
-                    <li><a class="dropdown-item" href="#">Belleza</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="./html/shoopCart.html">
-                    <span class="material-symbols-outlined">shopping_cart</span>
-                </a>
-            </li>
-            <li>
-                <button class="login-button">
-                    <a class="a-login" href="./html/login.html">Login</a>
-                </button>
-            </li>
-        </ul>
-    </nav>
-</header>
+    <header class="head">
+        <h2 class="title">Mystique<span class="span-title">Shop</span></h2>
+        <input type="search" placeholder="Busca productos, marcas y mas" id="Buscador" />
+        <nav class="navbar-home">
+            <ul class="ul-nav">
+                <li><a href="./index.html">Inicio</a></li>
+                <li><a href="./html/helpcenter.html">Soporte</a></li>
+                <ul class="nav-li">
+                    <li class="nav-item dropdown">
+                      <button class="button-drop"  aria-expanded="false">
+                        Articulos
+                      </button>
+                      <ul class="dropdown-menu dropdown-menu-dark">
+                        <li><a class="dropdown-item" href="./html/Cards.html">Moda</a></li>
+                        <li><a class="dropdown-item" href="#">Joyas</a></li>
+                        <li><a class="dropdown-item" href="#">Accesorios y bolsos</a></li>
+                        <li><a class="dropdown-item" href="#">Belleza</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                <li><a href="./html/shoopCart.html"><span class="material-symbols-outlined">
+                    shopping_cart
+                    </span></a>
+                </li>
+                    <button class="login-button">
+                        <a class="a-login" href="./html/login.html">Login</a>
+                    </button>
+            </ul>
+        </nav>
+    </header>
 
 `
 
